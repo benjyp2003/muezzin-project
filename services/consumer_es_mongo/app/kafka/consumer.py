865818847bb3f3d4236ddb997ca5584ecb700160
benjyp2003 <benjypfeffer@gmail.com>
2025-09-07 17:59:12 +0300

    Created consumer for cunsuming topics from the producer

---
+++ services/consumer_es_mongo/app/kafka/consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+import json, os
+
+
+class Consumer:
+    def __init__(self):
+        self.broker = os.getenv("BROKER", "localhost:9092")
+
+    def get_consumer_events(self, topic):
+        consumer = KafkaConsumer(topic,
+                                 group_id="consumer_es_mongo-group",
+                                 value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                                 bootstrap_servers=self.broker,
+                                 request_timeout_ms=30000,
+                                 api_version=(2, 0, 2))
+        return consumer
