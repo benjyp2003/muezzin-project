629ccb99c2c36713befa2a5961c38c6c26bf0343
benjyp2003 <benjypfeffer@gmail.com>
2025-09-08 16:17:23 +0300

    Refactored unique id generating so it generates the ID based on the file size concenated with a random number.

--- services/consumer_es_mongo/app/features/generate_id.py
+++ services/consumer_es_mongo/app/features/generate_id.py
@@ -1,15 +1,12 @@
 import random
-from datetime import datetime
 
-def generate_unique_id_with_timestamp(timestamp='1979-12-31 23:00:00.000'):
-    """Generates a unique ID based on the modified date (with miliseconds) and a random number"""
+def generate_unique_id(size):
+    """Generates a unique ID based on the file size and a random number"""
     try:
-        date_object = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
-        # Generate a random number to ensure uniqueness within the same millisecond
-        random_number = random.SystemRandom().randint(0, 99999)
+        random_number = random.SystemRandom().randint(0, 9999)
 
-        # Combine the timestamp and random component into a string
-        unique_id = f"{date_object}-{random_number}"
+        # Combine the size and random component into a string
+        unique_id = f"{size}-{random_number}"
         return unique_id
 
     except Exception as e:
